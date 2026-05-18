80fca1e7bdd6328dca5011fb75cf68fe1c824501
nasaige <qq450850523@gmail.com>
2026-05-19 02:11:17 +0800

    Upgrade English learning app to Pro version

--- english-ios-app/service-worker.js
+++ english-ios-app/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'english-learning-ios-v1';
+const CACHE_NAME = 'english-learning-pro-v2';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); self.clients.claim(); });
